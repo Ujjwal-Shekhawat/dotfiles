fd2a969e05e02a0b43b33b720597577edf398cc3
Ujjwal-Shekhawat <ujjus001@gmail.com>
2021-08-08 18:50:37 +0530

    imprived mpd and ncmpp notifications

--- mpdinoti.sh
+++ mpdinoti.sh
@@ -1,3 +1,4 @@
 x=$(echo "/home/$USER/Music/$(mpc -p 6601 -f %file%)" | grep -i $USER | cut -d '/' -f 1-5)
 x=$x"/cover.png"
-dunstify "Album Image" -i $x
+y=echo "Now Playing: $(mpc -p 6601 | head -1)"
+dunstify "Now Playing: $(mpc -p 6601 | head -1)" -i $x
